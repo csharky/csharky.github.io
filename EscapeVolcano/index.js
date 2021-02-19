fc1e1545b2c33b2cf29f5c506b718badc44bfe61
Max <me@cshark.xyz>
2021-02-19 17:14:10 +0300

    some shit

---
+++ EscapeVolcano/index.js
@@ -0,0 +1,11 @@
+import bridge from '@vkontakte/vk-bridge';
+
+bridge.send("VKWebAppInit", {});
+
+function vkRecordPlayerScore(score)
+{
+    console.log("wassup");ы
+    /*bridge.send("VKWebAppShowLeaderBoardBox", {user_result:score})
+         .then(data => console.log(data.success))
+        .catch(error => console.log(error));*/
+}
