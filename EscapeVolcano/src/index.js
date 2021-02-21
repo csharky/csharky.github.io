bcea4d754aa14a73b87e6b2c9d18b36a9ae02f44
Max <me@cshark.xyz>
2021-02-21 10:24:06 +0300

    updated vk shit

--- EscapeVolcano/src/index.js
+++ EscapeVolcano/src/index.js
@@ -1,14 +1,36 @@
 import bridge from '@vkontakte/vk-bridge';
+import UrlParser from './UrlParser'
 
 bridge.send("VKWebAppInit", {});
 
 function vkRecordPlayerScore(score)
 {
+    const parser = new UrlParser();
+    parser.parseUri();
+
+    try {
+        const access_token = parser.getParam('access_token');
+        const user_id = parser.getParam('viewer_id');
+
+        bridge
+        .send("VKWebAppApi", {
+            "method": "secure.addAppEvent",
+            "requestId": "updatedPlayerScore",
+            "params": 
+            {
+                "user_id": user_id,
+                "access_token": access_token,
+                "activity_id": "2",
+                "value": score
+            }
+        })
+        .then(data => console.log(data.success))
+        .catch(error => console.log(error));
+    } catch (e) {}
+
     bridge.send("VKWebAppShowLeaderBoardBox", {user_result:score})
          .then(data => console.log(data.success))
         .catch(error => console.log(error));
-
-    //ыUrlParser.
 }
 
 window.vkRecordPlayerScore = vkRecordPlayerScore;
